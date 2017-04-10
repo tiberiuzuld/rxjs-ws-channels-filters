d5eca488623e56eccffe8a5a847c31e6487f37bf
Tiberiu Zuld <tiberiuzuld@gmail.com>
2017-04-10 14:59:23 +0300

    add working filters

--- app/index.js
+++ app/index.js
@@ -1,7 +1,19 @@
 (function () {
   'use strict';
 
-  var socket = rxSocket.create({url: 'localhost:3000/api'});
+  var options = {
+    url: 'localhost:3000/api',
+    invalidUrl: function (e) {
+      return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+          options.url = 'localhost:3000/api';
+          resolve(10);
+        }, 1000);
+      });
+    }
+  };
+
+  var socket = rxSocket.create(options);
   console.log(socket);
   socket.channels.subscribe('one', function (message) {
     console.log(message);
@@ -15,4 +27,15 @@
   socket.channels.subscribe('two', function (message) {
     console.log(message);
   });
+
+  var filterSub = socket.channels.subscribeFilter('three', {id: 3}, function (message) {
+    console.log('filter', message);
+  });
+
+  setTimeout(function () {
+    filterSub.unsubscribe();
+    var filterSub2 = socket.channels.subscribeFilter('three', {id: 4}, function (message) {
+      console.log('filter', message);
+    });
+  }, 5000);
 })();
